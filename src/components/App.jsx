0616f3b4819e53aaff0ba9c3767bf846e3cd7202
Evan Katz <evankatz14@gmail.com>
2021-04-18 22:30:47 -0700

    initial router set up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import sum from '../helperFunctions/sum';
+import {useParams} from 'react-router-dom'
 
 // const sum = require('../helperFunctions/sum')
 
 export default function App(props) {
+  let {id} = useParams();
   let thing = sum(1,2)
   return(
-    <div>Hello World {thing}</div>
+    <div>Hello World {id}</div>
   )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,32 @@
 import React from 'react';
 import {render} from 'react-dom';
 import App from './components/App.jsx';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
-render(<App />, document.getElementById('app'));
+export default function Routes() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/13023">Start</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/:id" children={<App />} />
+        </Switch>
+      </div>
+    </Router>
+  )
+
+}
+
+render(<Routes />, document.getElementById('app'));
