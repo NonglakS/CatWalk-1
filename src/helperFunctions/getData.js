724f1d507a139efaa4bd3b18d9b10d5fb92f05c3
Evan Katz <evankatz14@gmail.com>
2021-04-20 09:36:57 -0700

    update token usage

--- src/helperFunctions/getData.js
+++ src/helperFunctions/getData.js
@@ -3,7 +3,7 @@ import TOKEN from '../../config.js';
 
 export default function getData(urlAddOn, callback) {
   axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sjo/${urlAddOn}`, {
-    headers: { Authorization: TOKEN.TOKEN },
+    headers: { Authorization: TOKEN },
   })
     .then((res) => callback(null, res))
     .catch((err) => callback(err));
