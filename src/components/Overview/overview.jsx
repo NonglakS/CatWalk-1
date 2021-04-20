71e01281b4cb7f90499d7ead2f7577bebb5ea432
Nonglak Saeueng <mam.nonglaks@gmail.com>
2021-04-20 01:09:57 -0700

    render icon selector

--- src/components/Overview/overview.jsx
+++ src/components/Overview/overview.jsx
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import getData from '../../helperFunctions/getData.js';
 import Gallery from './gallery.jsx'
+import StyleSelector from './styleSelector.jsx'
 
-function Overview () {
+
+export const StyleContext = React.createContext();
+export const CurrentStyleContext = React.createContext();
+
+export default function Overview () {
+
+  const [styles, setStyles] = useState('');
+  const [currentStyle, setCurrentStyle] = useState('');
+
+  useEffect(()=>{
+    let getStyleUrl = `products/13023/styles`;
+    getData (getStyleUrl, (err, res) => {
+      err ? console.log('ERROR', err) :  setStyles(res.data.results);
+    })
+  }, [])
+
+
+
+  useEffect(()=>{
+    let getStyleUrl = `products/13023/styles`
+    getData(getStyleUrl, (err, res) => {
+      err ? console.log('ERROR', err) :  setCurrentStyle(res.data.results[0]);
+    })
+  }, [])
+
+
   return (
     <div className="overview container ">
+      <StyleContext.Provider value={styles}>
+      <CurrentStyleContext.Provider value={currentStyle}>
       <div className="row mainview">
         <div className="col-md-7 my-auto d-flex justify-content-center"><Gallery /></div>
-        <div className="col">Product Information</div>
+        <div className="col">Product Information
+        <StyleSelector /></div>
       </div>
+      </CurrentStyleContext.Provider>
+      </StyleContext.Provider>
     </div>
     );
 }
 
-export default Overview;
---
+++ src/components/Overview/styleSelector.jsx
@@ -0,0 +1,61 @@
+import React, {useState} from 'react';
+import { StyleContext, CurrentStyleContext} from './overview.jsx'
+
+function StyleSelector () {
+
+
+  return (
+    <StyleContext.Consumer>
+      { styles => {
+          return (
+            <div className="style-selector row">
+              <Circle styles={styles}/>
+          </div>)
+        }
+      }
+    </StyleContext.Consumer>
+  )
+}
+
+
+export default StyleSelector;
+
+function Circle ({styles}) {
+
+  const [select, setSelect] = useState(false)
+
+  var circleStyle = {
+    width: '80px',
+    height: '80px',
+    borderRadius: '40px'
+  }
+
+  var tickStyle = {
+    width: '30px',
+    position: 'absolute',
+    right: '5px'
+  }
+
+  var handleSelect = () => {
+    setSelect(!select)
+  }
+
+  return (
+    <div className="circle container">
+      <div className="row">
+        { typeof styles!== 'string' && styles.map((style => {
+          return (
+            <div className="col-md-3">
+              { select &&
+              <div className="tickmark">
+                <img src="https://img.icons8.com/pastel-glyph/64/000000/checked--v1.png" style={tickStyle}/>
+              </div>
+              }
+                <img src={style.photos[0].thumbnail_url} style={circleStyle} onClick={handleSelect}/>
+            </div>
+          )
+        }))}
+      </div>
+     </div>
+  )
+}
