b1a20d5fa15f36ab882cc034797b64b7bda2efd2
Ben Pack <benpack007@gmail.com>
2021-04-20 17:13:48 -0700

    Implimented answer body in Answers.jsx.

--- src/components/QA/Answers.jsx
+++ src/components/QA/Answers.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
+import Moment from 'moment'
 
 function Answers({ answer }) {
+  const helpfulButton = {
+    border: 'none',
+    backgroundColor: 'white',
+    textDecoration: 'underline',
+  };
 
+  const dateConverter = function (date) {
+
+  };
 
   return (
-    <span>
-      A: Who cares?
+    <div>
+      <text>
+        A:
+        {' '}
+        {answer.body}
+        <br />
+      </text>
+      <text>
+        {answer.answerer_name}
+        {' '}
+        {Moment(answer.date).format("MMMM Do YYYY")}
+      </text>
+      <div>
+        | Helpful?
+        <button type="submit" style={helpfulButton}> Yes </button>
+        <text>
+          ({answer.helpfulness})
+        </text>
+        <button type="submit" style={helpfulButton}> Report </button>
+      </div>
       <br />
-    </span>
+    </div>
   );
 }
 
--- src/components/QA/Questions.jsx
+++ src/components/QA/Questions.jsx
@@ -1,37 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Answers from './Answers.jsx';
 
-function Questions({question}) {
-
+function Questions({ question }) {
+  const urlAddOn = 'qa/questions?product_id=13025';
+  const [allAnswers, setAllAnswers] = useState('');
+  const [answersRendered, setAnswersRendered] = useState(4);
+  const [displayedAnswers, setDisplayedAnswers] = useState('');
   const helpfulButton = {
-    border: "none",
-    backgroundColor: "white",
-    textDecoration: "underline"
+    border: 'none',
+    backgroundColor: 'white',
+    textDecoration: 'underline',
   };
+  const getAnswers = function(questionObject) {
+    const answerKeys = Object.values(questionObject.answers);
+    setAllAnswers(answerKeys)
+  }
 
-  const renderAnswers = function (questionArray) {
-    const [answersRendered, setAnswersRendered] = useState(2);
-    const [displayedAnswers, setDisplayedAnswers] = useState('');
-    const answers = [];
-    for (let i = 0; i < answersRendered; i++) {
-      if (questionArray[i] === undefined) {
-        return;
-      }
-      answers.push(questionArray[i].answers);
-    }
-    setAnswersRendered(answersRendered + 2);
-    setDisplayedAnswers(answers);
-  };
+  useEffect(() => {
+    getAnswers(question)
+    setDisplayedAnswers(allAnswers.slice(0, 2))
+  });
+
+  const test = function() {
+    console.log(allAnswers)
+    console.log(displayedAnswers)
+  }
 
   return (
     <span>
-      Q: {question.question_body}
+
+      <h4> Q: {question.question_body}</h4>
       <p>
         Helpful?
         <button type="submit" style={helpfulButton}> Yes </button>
       </p>
-      <Answers answer={question.answers} />
-      <button type="submit" onClick={() => renderAnswers(allQuestions.results)}> MORE ANSWERS </button>
+      {displayedAnswers
+      && displayedAnswers.map((data) => <Answers key={data.toString()} answer={data} />)}
+      <br />
+      <button type="submit" onClick={() => test()}> MORE ANSWERS </button>
       <br />
     </span>
   );
