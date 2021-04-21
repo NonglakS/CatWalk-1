779d4107daed8ebe5e6f7bc1765c5bc82fe896f9
Nonglak Saeueng <mam.nonglaks@gmail.com>
2021-04-21 11:14:28 -0700

    add jest config file

--- src/testing/api/api.test.js
+++ src/testing/api/api.test.js
@@ -1,13 +1,12 @@
 const request = require("supertest")("https://app-hrsei-api.herokuapp.com/api/fec2/hr-sjo");
 
-
 describe("GET /products", function () {
   it("returns 5 products at a time", async function () {
 
     const response = await request.get("/products")
-    .set('Authorization', 'my secret');
 
-    console.log('response-----', response.status)
+    .set('Authorization', process.env.TOKEN);
+
       expect(response.status).toBe(200);
       expect(response.body.length).toBe(5);
 
