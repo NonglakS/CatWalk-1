decbf1a2e0f8db2ebd0b5c835b79f9afa865374b
Nonglak Saeueng <mam.nonglaks@gmail.com>
2021-04-20 17:21:31 -0700

    delete console.log

--- src/helperFunctions/getData.js
+++ src/helperFunctions/getData.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-console.log(process.env)
-
 export default function getData(urlAddOn, callback) {
   axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sjo/${urlAddOn}`, {
     headers: { Authorization: process.env.TOKEN },
