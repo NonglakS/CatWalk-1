851a0b1cb47b08ad990af570b42ab1b490e92da4
Nonglak Saeueng <mam.nonglaks@gmail.com>
2021-04-21 11:19:45 -0700

    import config file

--- src/testing/api/api.test.js
+++ src/testing/api/api.test.js
@@ -1,12 +1,12 @@
 const request = require("supertest")("https://app-hrsei-api.herokuapp.com/api/fec2/hr-sjo");
+require("dotenv").config()
 
 describe("GET /products", function () {
   it("returns 5 products at a time", async function () {
 
     const response = await request.get("/products")
-
     .set('Authorization', process.env.TOKEN);
-
+    
       expect(response.status).toBe(200);
       expect(response.body.length).toBe(5);
 
