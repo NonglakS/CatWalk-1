e15aef302e1036a8de1d17d999e4600ee3887a88
Ben Pack <benpack007@gmail.com>
2021-04-21 10:17:54 -0700

    bundle.js

--- src/helperFunctions/getData.js
+++ src/helperFunctions/getData.js
@@ -2,11 +2,7 @@ import axios from 'axios';
 
 export default function getData(urlAddOn, callback) {
   axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sjo/${urlAddOn}`, {
-<<<<<<< HEAD
-    headers: { Authorization: TOKEN },
-=======
     headers: { Authorization: process.env.TOKEN },
->>>>>>> master
   })
     .then((res) => callback(null, res))
     .catch((err) => callback(err));
