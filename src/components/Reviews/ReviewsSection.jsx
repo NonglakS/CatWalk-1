abbe88ba38466df1e74f5ab4ba91269bff721645
Evan Katz <evankatz14@gmail.com>
2021-04-22 14:07:47 -0700

    Add Reviews modal

--- src/components/Reviews/ReviewsSection.jsx
+++ src/components/Reviews/ReviewsSection.jsx
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import getData from '../../helperFunctions/getData.js';
 import ReviewTile from './ReviewTile.jsx';
+import Modal from '../../shared-components/Modal.jsx';
 
 export default function ReviewsSection({ reviewsMeta }) {
   const [reviews, setReviews] = useState([]);
   const [reviewCount, setReviewCount] = useState(2);
   const [renderedReviews, setRenderedReviews] = useState([]);
+  const modal = useRef(null);
 
   useEffect(() => {
     getData('reviews?product_id=13023&count=10000', (err, res) => {
@@ -46,6 +48,18 @@ export default function ReviewsSection({ reviewsMeta }) {
         {reviews.results && reviewCount < reviews.results.length && (
           <button className="show-more-btn" type="button" onClick={() => rerenderReviews()}>More Reviews</button>
         )}
+        <button type="button" onClick={() => modal.current.open()}>
+          Add Review
+        </button>
+        <Modal ref={modal} fade>
+        <form>
+          <label>
+            Name:
+            <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        </Modal>
       </div>
     </div>
   );
