e82cbc302b2947659dcc6b81fa74b5022356d954
Evan Katz <evankatz14@gmail.com>
2021-04-21 21:42:45 -0700

    modal

--- src/components/Reviews/ReviewPic.jsx
+++ src/components/Reviews/ReviewPic.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import Modal from '../../shared-components/Modal.jsx';
 
-export default function ReactPic({url}) {
+export default function ReactPic({ url }) {
+  const modal = useRef(null);
+
   return (
-    <img className="thumbnail" src={url} alt="image associated with review" style={ {height: '75px', width: '75px'} }/>
-  )
+    <>
+      <button type="button" style={{ padding: '0px', margin: '10px', border: 'none' }} onClick={() => modal.current.open()}>
+        <img className="thumbnail" src={url} alt="associated with review" style={{ height: '75px', width: '75px' }} />
+      </button>
+      <Modal ref={modal} fade>
+        <img src={url} alt="" style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto' }}/>
+      </Modal>
+    </>
+  );
 }
