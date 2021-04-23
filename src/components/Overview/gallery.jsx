a3885e881fe4d806d8b1700e23190750a38f5912
Nonglak Saeueng <mam.nonglaks@gmail.com>
2021-04-23 11:04:43 -0700

    render carousel dynamically

--- src/components/Overview/gallery.jsx
+++ src/components/Overview/gallery.jsx
@@ -16,42 +16,21 @@ function Gallery({ currentStyle }) {
     <div className="main-image" >
       <div className="thumbnails"><Thumbnails /></div>
       <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={currentStyle.photos[0].url}
-            alt="First slide"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={currentStyle.photos[2].url}
-            alt="Second slide"
-          />
-
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={currentStyle.photos[1].url}
-            alt="Third slide"
-          />
-
-        </Carousel.Item>
+        {currentStyle.photos.map((photo) => {
+          return (
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={photo.url}
+                alt={`image of ${currentStyle.name}`}
+              />
+            </Carousel.Item>
+          )
+        })}
       </Carousel>
-
     </div>
   )
-
 }
 
 export default Gallery;
 
-    //   <button id="glider-prev">
-    //   <FaAngleLeft />
-    //   </button>
-    //   <img src={currentStyle.photos[0].url} alt="product description" style={imgStyle} class="img-fluid" alt="Responsive image"/>
-    //   <button id="glider-next">
-    //  <FaAngleRight />
-    //   </button>
