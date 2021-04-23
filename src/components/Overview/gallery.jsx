5e847d31988a850266d7e8255ed07a451d2e3b5d
Nonglak Saeueng <mam.nonglaks@gmail.com>
2021-04-23 15:48:02 -0700

    align thumbnails in the middle

--- src/components/Overview/gallery.jsx
+++ src/components/Overview/gallery.jsx
@@ -2,23 +2,14 @@ import React from 'react';
 import Thumbnails from './thumbnails.jsx'
 import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 import { Carousel } from 'react-bootstrap';
-// import Vertical from './vertical.jsx'
-// import { Carousel } from 'react-responsive-carousel';
-// import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 
 function Gallery({ currentStyle }) {
 
-  // const imgStyle = {
-
-  //   'max-height': '612px',
-  //   'text-align': 'center'
-
-  // }
-
   return (
-    <div className="main-image" >
+    <div className="main-image container" >
 
-      {/* <div className="thumbnails"><Thumbnails /></div> */}
+      <div className="thumbnails d-flex h-100 align-items-center justify-content-center align-middle "><Thumbnails currentStyle={currentStyle} /></div>
       <Carousel interval={null}>
         {currentStyle.photos.map((photo) => {
           return (
@@ -39,4 +30,3 @@ function Gallery({ currentStyle }) {
 }
 
 export default Gallery;
-
--- src/components/Overview/thumbnails.jsx
+++ src/components/Overview/thumbnails.jsx
@@ -1,25 +1,8 @@
-import React,  {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import getData from '../../helperFunctions/getData.js';
 
 
-
-// future inplementation,  pass style id as a props.
-function Thumbnails () {
-
-  const [thumbnails, setThumbnails] =  useState([]);
-  const [offScale, setOffScale] = useState(false);
-
-  useEffect(()=>{
-    let getStyleUrl = `products/13023/styles`
-    getData(getStyleUrl, (err, res) => {
-      err ? console.log('ERROR', err) : setThumbnails(res.data.results[0].photos);
-      // if there's more 7 thumbnails, have arrow buttons to scroll up-down
-      if (res.data.results[0].photos.length > 7) {
-        setOffScale(true)
-      }
-    })
-  }, [])
-
+function Thumbnails({currentStyle}) {
 
   var style_img = {
     height: '75px',
@@ -31,43 +14,27 @@ function Thumbnails () {
     border: '0'
   }
 
-
   return (
     <div>
-      { offScale && <UpButton /> }
-      <ul>
-        {thumbnails.slice(0,6).map(photo => {
-          return (
-            <li style={style_thumbnail}>
-              <button className="button-thumbnail">
-                <img src={photo.thumbnail_url} alt="" style={style_img}/>
-              </button>
-            </li>
+      {/* { offScale && <UpButton /> } */}
+
+      {currentStyle.photos !== undefined &&
+
+        <ul>
+          {currentStyle.photos.map(photo => {
+            return (
+              <li style={style_thumbnail}>
+                <button className="button-thumbnail">
+                  <img src={photo.thumbnail_url} alt="" style={style_img} />
+                </button>
+              </li>
             )
-        })}
-      </ul>
-      { offScale && <DownButton /> }
+          })}
+        </ul>
+      }
+      {/* { offScale && <DownButton /> } */}
     </div>
   )
 }
-
-
-function UpButton () {
-  return (
-    <button id="button-up">
-      <img src="https://img.icons8.com/android/24/000000/sort-up.png"/>
-    </button>
-  )
-}
-
-
-function DownButton () {
-  return (
-    <button id="button-up">
-      <img src="https://img.icons8.com/android/24/000000/sort-up.png"/>
-    </button>
-  )
-}
-
 
 export default Thumbnails;
