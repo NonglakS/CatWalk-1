381f2704aaa9f11a00e9bb1d32cb055b6d049a82
Nonglak Saeueng <mam.nonglaks@gmail.com>
2021-04-22 17:37:48 -0700

    add handle qty change

--- src/components/Overview/cart.jsx
+++ src/components/Overview/cart.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import Select from "react-dropdown-select";
+import Select from "react-select";
+
 
 
 function Cart({ currentStyle }) {
@@ -7,6 +8,8 @@ function Cart({ currentStyle }) {
   const [sizeOptions, setSizeOptions] = useState([]);
   const [qtyOptions, setQtyOptions] = useState([]);
   const [outOfStock, setOutOfStock] = useState(false);
+  const [disable, setDisable] = useState(true);
+  const [currentVal, setCurrentVal] = useState({ label: 1, value: 1 })
 
   const tempSize = [];
 
@@ -23,19 +26,25 @@ function Cart({ currentStyle }) {
   }, [currentStyle])
 
   var handleSizeChange = (e) => {
-
-    if (e[0].value < 15) {
-      setQtyOptions(updateRange(e[0].value));
+    setDisable(false);
+    setCurrentVal({ label: 1, value: 1 });
+    if (e.value < 15) {
+      setQtyOptions(updateRange(e.value));
     } else {
       setQtyOptions(updateRange(15))
     }
+  }
 
+  var handleQtyChange = (e) => {
+    setCurrentVal(e)
   }
 
   var updateRange = (max) => {
     var arr = Array.from({ length: max }, (val, index) => index + 1);
     return arr.map((val) => { return { value: val, label: val } });
   }
+
+
 
   return (
     <>
@@ -44,7 +53,10 @@ function Cart({ currentStyle }) {
           <Select options={sizeOptions} placeholder={outOfStock ? "Out of Stock" : "Select size"} onChange={handleSizeChange} />
         </div>
         <div className="col-4 select-quantity">
-          <Select options={qtyOptions} placeholder="-" /></div>
+          {!disable
+            ? <Select options={qtyOptions} value={currentVal} onChange={handleQtyChange} />
+            : <Select options={qtyOptions} placeholder="-" isDisabled={disable} />}
+        </div>
       </div>
       <div className="row cart">
         <div className="col-10 add-to-bag ">select-size</div>
