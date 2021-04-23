36c73a3ca34a2c024d23c7589864216f9d4afc6c
Nonglak Saeueng <mam.nonglaks@gmail.com>
2021-04-23 01:21:22 -0700

    add main image

--- src/components/Overview/gallery.jsx
+++ src/components/Overview/gallery.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import Thumbnails from './thumbnails.jsx'
 
-function  Gallery () {
+function Gallery({ currentStyle }) {
+
+  const imgStyle = {
+    // 'max-width': '612px',
+    'max-height': '612px',
+    'text-align': 'center'
+
+  }
 
   return (
-
     <div className="main-image" >
-      <div className="thumbnails"><Thumbnails/></div>
+      <div className="thumbnails"><Thumbnails /></div>
+      <button id="glider-prev">
+
+      </button>
+      <img src={currentStyle.photos[0].url} alt="product description" style={imgStyle} class="img-fluid" alt="Responsive image"/>
+      <button id="glider-next">
+
+      </button>
+
     </div>
   )
 
--- src/components/Overview/overview.jsx
+++ src/components/Overview/overview.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import getData from '../../helperFunctions/getData.js';
 import Gallery from './gallery.jsx';
 import StyleSelector from './styleSelector.jsx';
@@ -7,30 +7,30 @@ import ProductOverview from './productOverview.jsx'
 import ShareIcon from './shareIcon.jsx'
 
 
-export default function Overview ({product}) {
+export default function Overview({ product }) {
 
   const [styles, setStyles] = useState('');
   const [currentStyle, setCurrentStyle] = useState('');
   const [select, setSelect] = useState('');
 
 
-  useEffect(()=>{
+  useEffect(() => {
 
-      let getStyleUrl = `products/${product.id}/styles`;
-      getData (getStyleUrl, (err, res) => {
-        if (err) {
-          console.log('ERROR', err)
-        } else {
-          setStyles(res.data.results);
-          setCurrentStyle(res.data.results[0]);
-          setSelect(`tick_${res.data.results[0].style_id}`)
+    let getStyleUrl = `products/${product.id}/styles`;
+    getData(getStyleUrl, (err, res) => {
+      if (err) {
+        console.log('ERROR', err)
+      } else {
+        setStyles(res.data.results);
+        setCurrentStyle(res.data.results[0]);
+        setSelect(`tick_${res.data.results[0].style_id}`)
 
-        }
-      })
+      }
+    })
   }, []);
 
 
-  function handleStyleChange (newStyle){
+  function handleStyleChange(newStyle) {
     setCurrentStyle(newStyle)
     setSelect(`tick_${newStyle.style_id}`)
   }
@@ -38,24 +38,27 @@ export default function Overview ({product}) {
   return (
     <div className="overview container ">
       <div className="row mainview">
-        <div className="col-md-7 my-auto d-flex justify-content-center"><Gallery /></div>
+        <div className="col-md-7 my-auto d-flex justify-content-center">
+          {currentStyle &&
+            <Gallery currentStyle={currentStyle} />}
+        </div>
         <div className="col product-information">
           <ProductInfo
             product={product}
             styles={styles}
-            currentStyle={currentStyle}/>
+            currentStyle={currentStyle} />
           <StyleSelector
             handleStyleChange={handleStyleChange}
             styles={styles}
             currentStyle={currentStyle}
             select={select}
-             />
-            <ShareIcon />
+          />
+          <ShareIcon />
         </div>
       </div>
       <div className="product-overview">
         <ProductOverview
-        product={product}/>
+          product={product} />
       </div>
     </div>
   );
