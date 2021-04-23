06099abb4d29093299c95b8ab7f7244f51cdea30
Nonglak Saeueng <mam.nonglaks@gmail.com>
2021-04-23 00:04:12 -0700

    make the size dropdown open when add to cart with empty item

--- src/components/Overview/cart.jsx
+++ src/components/Overview/cart.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
 import Select from "react-select";
 import { Button, Icon } from 'semantic-ui-react';
 import axios from 'axios';
@@ -10,7 +11,8 @@ function Cart({ currentStyle }) {
   const [qtyOptions, setQtyOptions] = useState([]);
   const [outOfStock, setOutOfStock] = useState(false);
   const [disable, setDisable] = useState(true);
-  const [currentVal, setCurrentVal] = useState({ label: 1, value: 0, sku: "" })
+  const [currentVal, setCurrentVal] = useState({ label: 1, value: 0, sku: "" });
+  const [clickAdd, setClickAdd] = useState(false)
 
   const tempSize = [];
 
@@ -28,6 +30,9 @@ function Cart({ currentStyle }) {
 
   var handleSizeChange = (e) => {
     setDisable(false);
+    setClickAdd(false)
+
+
     setCurrentVal({ label: 1, value: 1, sku: e.sku });
     if (e.value < 15) {
       setQtyOptions(updateRange(e.value));
@@ -47,8 +52,8 @@ function Cart({ currentStyle }) {
 
   var handleAddToCart = (e) => {
     if (!currentVal.value) {
-      console.log('Please select size above the dropdown.')
-      //trigger drop down to open
+      setClickAdd(true)
+
     } else {
 
       const options = {
@@ -61,7 +66,7 @@ function Cart({ currentStyle }) {
         headers: { Authorization: process.env.TOKEN }
       };
 
-      axios(options).then(res => console.log("response", res.status))
+      axios(options).then(res => console.log("Added an item to cart! Response code : ", res.status))
 
     }
   }
@@ -69,9 +74,12 @@ function Cart({ currentStyle }) {
 
   return (
     <>
+      <div id="select-size-reminder">{clickAdd ? 'Please Select Size !' : <br />}</div>
       <div className="row  size-qty-selector">
         <div className="col-8 select-size">
-          <Select id="size-options" options={sizeOptions} placeholder={outOfStock ? "Out of Stock" : "SELECT SIZE"} onChange={handleSizeChange} />
+          {clickAdd
+            ? <Select id="size-options" menuIsOpen={true} options={sizeOptions} onChange={handleSizeChange} />
+            : <Select id="size-options" options={sizeOptions} placeholder={outOfStock ? "Out of Stock" : "SELECT SIZE"} onChange={handleSizeChange} />}
         </div>
         <div className="col-4 select-quantity">
           {!disable
@@ -80,15 +88,16 @@ function Cart({ currentStyle }) {
         </div>
       </div>
       <div className="row cart">
-        <div className="col-9 add-to-bag " id="add">
+        <div className="col add-to-bag " id="add">
 
           <Button basic icon='plus' content='ADD TO CART' labelPosition='right' onClick={handleAddToCart} disabled={outOfStock ? true : false} />
         </div>
-        <div className="col add-to-collection" id="fav">
+        <div className="col d-flex flex-row-reverse add-to-collection" id="fav">
           <Button basic>
             <Icon name='star outline' />
           </Button></div>
       </div>
+
     </>
   )
 }
