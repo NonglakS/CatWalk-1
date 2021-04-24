e8b8629e2d29293e4ef31797987181a61308143a
Evan Katz <evankatz14@gmail.com>
2021-04-24 15:40:59 -0700

    recommended review percentage

--- src/components/Reviews/Ratings.jsx
+++ src/components/Reviews/Ratings.jsx
@@ -1,14 +1,19 @@
+/* eslint-disable max-len */
 import React from 'react';
 import { FaRegStar, FaStar, FaStarHalfAlt } from 'react-icons/fa';
 import makeAverageStars from '../../helperFunctions/makeAverageStars.js';
+import getReviewCount from '../../helperFunctions/getReviewCount.js';
+import getRecommendPercent from '../../helperFunctions/getRecommendPercent.js';
 
 export default function Ratings({ reviewsMeta, reviewScore }) {
-  // const starRating = makeAverageStars(reviewScore);
-  const starRating = [1,1,2,0,0];
+  const starRating = makeAverageStars(reviewScore);
+  const reviewCount = getReviewCount(reviewsMeta.ratings);
+  const recommendPercent = reviewsMeta.recommended ? getRecommendPercent(reviewsMeta.recommended.true, reviewCount) : 0;
+  console.log('recommendPercent', recommendPercent);
   return (
     <>
       <div style={{ display: 'flex' }}>
-        <h1 style={{ marginRight: '5px'}}>{reviewScore}</h1>
+        <h1 style={{ marginRight: '5px' }}>{reviewScore}</h1>
         <div>
           {starRating.map((star) => {
             if (star === 0) {
@@ -19,7 +24,14 @@ export default function Ratings({ reviewsMeta, reviewScore }) {
             return <FaStarHalfAlt />;
           })}
         </div>
+        <div>
+          (
+          {reviewCount}
+          {' '}
+          reviews)
+        </div>
       </div>
+      <div>{recommendPercent}% of reviews recommend this product</div>
       {reviewsMeta.characteristics && (
         Object.keys(reviewsMeta.characteristics).map((key) => (
           <div>
