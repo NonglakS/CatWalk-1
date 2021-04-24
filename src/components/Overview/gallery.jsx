907e6d01f33c9546f8de6e286d84dc6725b7c991
Nonglak Saeueng <mam.nonglaks@gmail.com>
2021-04-23 18:56:55 -0700

    lset activeIndex to thumbnail

--- src/components/Overview/gallery.jsx
+++ src/components/Overview/gallery.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Thumbnails from './thumbnails.jsx'
 import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 import { Carousel } from 'react-bootstrap';
@@ -6,11 +6,25 @@ import { Carousel } from 'react-bootstrap';
 
 function Gallery({ currentStyle }) {
 
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setActiveIndex(selectedIndex);
+    console.log("active item change")
+  };
+
   return (
     <div className="main-image container" >
 
-      <div className="thumbnails d-flex h-100 align-items-center justify-content-center align-middle "><Thumbnails currentStyle={currentStyle} /></div>
-      <Carousel interval={null} onSelect={() => console.log("active item change")}>
+      <div className="thumbnails d-flex h-100 align-items-center justify-content-center align-middle ">
+        <Thumbnails currentStyle={currentStyle}
+        activeIndex={activeIndex} handleSelect={handleSelect} />
+      </div>
+      <Carousel
+        activeIndex={activeIndex}
+        interval={null}
+        onSelect={handleSelect}
+      >
         {currentStyle.photos.map((photo) => {
           return (
             <Carousel.Item style={{ 'height': "600px" }}>
--- src/components/Overview/thumbnails.jsx
+++ src/components/Overview/thumbnails.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import getData from '../../helperFunctions/getData.js';
 
 
-function Thumbnails({currentStyle}) {
+function Thumbnails({ currentStyle, handleSelect }) {
 
   var style_img = {
     height: '75px',
@@ -13,6 +13,10 @@ function Thumbnails({currentStyle}) {
     listStyleType: 'none',
     border: '0'
   }
+  var changeMainImage = (e, index) => {
+    handleSelect(index, e)
+
+  }
 
   return (
     <div>
@@ -21,11 +25,12 @@ function Thumbnails({currentStyle}) {
       {currentStyle.photos !== undefined &&
 
         <ul>
-          {currentStyle.photos.map(photo => {
+          {currentStyle.photos.map((photo, index) => {
             return (
               <li style={style_thumbnail}>
-                <button className="button-thumbnail">
-                  <img src={photo.thumbnail_url} alt="" style={style_img} />
+                <button id={index} className="button-thumbnail"
+                  onClick={(e)=>{changeMainImage(e, index)}}>
+                  <img src={photo.thumbnail_url} alt="thumbnail" style={style_img} />
                 </button>
               </li>
             )
