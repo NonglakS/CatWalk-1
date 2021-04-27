50ea65c2eeed5c55f618a6b3031151e9d00d2970
Evan Katz <evankatz14@gmail.com>
2021-04-27 13:13:36 -0700

    post new answer

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
+const bodyParser = require('body-parser');
 require("dotenv").config();
 
 const app = express();
+app.use(bodyParser.json());
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, '../public')));
@@ -30,6 +32,9 @@ app.get('/products/:id/styles', async (req, res) => {
   }
 });
 
+
+
+
 app.get('/qa/questions', async (req, res) => {
   try {
     const apiRes = await axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sjo/qa/questions?product_id=${req.query.product_id}&count=${req.query.count}`, {
@@ -41,23 +46,23 @@ app.get('/qa/questions', async (req, res) => {
   }
 });
 
-app.get('/reviews/meta', async (req, res) => {
+app.put('/qa/questions/:id/helpful', async (req, res) => {
   try {
-    const apiRes = await axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sjo/reviews/meta?product_id=${req.query.product_id}`, {
+    await axios.put(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sjo/qa/questions/${req.params.id}/helpful`, null, {
       headers: { Authorization: process.env.TOKEN },
     });
-    res.send(apiRes.data);
+    res.send('successfully marked question helpful');
   } catch (err) {
     res.send(err);
   }
 });
 
-app.get('/reviews', async (req, res) => {
+app.post('/qa/questions/:id/answers', async (req, res) => {
   try {
-    const apiRes = await axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sjo/reviews?product_id=${req.query.product_id}`, {
+    await axios.post(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sjo/qa/questions/${req.params.id}/answers`, req.body, {
       headers: { Authorization: process.env.TOKEN },
     });
-    res.send(apiRes.data);
+    res.send('successfully posted a new answer');
   } catch (err) {
     res.send(err);
   }
@@ -85,12 +90,28 @@ app.put('/qa/answers/:id/report', async (req, res) => {
   }
 });
 
-app.put('/qa/questions/:id/helpful', async (req, res) => {
+
+
+
+
+
+app.get('/reviews/meta', async (req, res) => {
   try {
-    await axios.put(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sjo/qa/questions/${req.params.id}/helpful`, null, {
+    const apiRes = await axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sjo/reviews/meta?product_id=${req.query.product_id}`, {
       headers: { Authorization: process.env.TOKEN },
     });
-    res.send('successfully marked question helpful');
+    res.send(apiRes.data);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+app.get('/reviews', async (req, res) => {
+  try {
+    const apiRes = await axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sjo/reviews?product_id=${req.query.product_id}`, {
+      headers: { Authorization: process.env.TOKEN },
+    });
+    res.send(apiRes.data);
   } catch (err) {
     res.send(err);
   }
@@ -117,6 +138,8 @@ app.put('/reviews/:id/report', async (req, res) => {
     res.send(err);
   }
 });
+
+
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'), (err) => {
